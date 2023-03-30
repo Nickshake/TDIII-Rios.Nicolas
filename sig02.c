685f623962eede8eb7161ddc2de77c735db453a1
Nickshake <nicolasrios2010@ymail.com>
2023-03-30 20:18:10 -0300

    prueba

---
+++ sig02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+/*void manejador_serial(int a){
+}*/
+
+int main()
+{
+	signal(SIGKILL,SIG_IGN);
+	//signal(SIGSTOP;SIG_IGN); //Ctrl+Z
+	printf("Proceso PID = %d\n",getpid());
+	
+	while (1);
+	
+	exit(0);
+}
